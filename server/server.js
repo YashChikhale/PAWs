0f9782acee664e243208471fedd1faea7694c152
YashChikhale <157518742+YashChikhale@users.noreply.github.com>
2025-01-17 20:30:45 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser')
+const cors = require('cors');
+const contactMess = require('./router/contact.router.js')
+const adoptingSec = require('./router/adopt.router.js')
+
+
+mongoose.connect(
+    "mongodb+srv://PAWs:MeowMeow@paws.36xog.mongodb.net/"
+).then(() => {console.log('MONGOBD CONNECTED')}).catch((error)=>{console.log(error)})
+
+const app = express()
+const PORT = process.env.PORT || 8000;
+
+// writing corrrsss
+app.use(
+    cors({
+        origin: 'http://127.0.0.1:5500',
+        methods:['GET','POST','PUT','DELETE'],
+        allowedHeaders:[
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma"
+        ],
+        credentials: true
+    })
+)
+app.use(cookieParser());
+app.use(express.json());
+app.use('/api/contact',contactMess);
+app.use('/api/adoption',adoptingSec);
+//app.use("/api/admin/products",adminProductRouter)
+//app.use("/api/shop/products",shopProductRouter)
+
+app.listen(PORT,()=> console.log(`server is running on :${PORT}`))
